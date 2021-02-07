f2c0e0b656bd083e49a74827d6bada33acb5b122
Damian Antonowicz <kontakt@damianantonowicz.pl>
2021-02-07 21:58:20 +0100

    Added setting DayControl.ControlTemplate from code behind.

--- XamarinForms.CalendarComponent/MainPage.xaml.cs
+++ XamarinForms.CalendarComponent/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinForms.CalendarComponent.Components;
 
 namespace XamarinForms.CalendarComponent
 {
@@ -13,6 +14,16 @@ namespace XamarinForms.CalendarComponent
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void CalendarControl_OnDayAdded(object sender, DayControlAddedEventArgs e)
+        {
+            if (e.DayControl.Date.DayOfWeek == DayOfWeek.Saturday ||
+                e.DayControl.Date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                e.DayControl.ControlTemplate = Resources["DayControlTemplate2"] as ControlTemplate;
+                e.DayControl.BindingContext = "weekend";
+            }
         }
     }
 }
